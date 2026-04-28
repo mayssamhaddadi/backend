af900ce1dc14ae532c9dba25cdfe3babe9b5e199
abdel <mayssamhaddadi@gmail.com>
2026-04-28 11:24:09 +0100

    fix railway port

--- app.js
+++ app.js
@@ -211,8 +211,8 @@ app.get("/health", (req, res) => {
 // =======================
 // 🚀 START SERVER
 // =======================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
-app.listen(PORT, "0.0.0.0", () => {
+app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
