bc41de174187cec1b6f206b8a996ac51d7e1e91a
abdel <mayssamhaddadi@gmail.com>
2026-04-28 10:51:00 +0100

    add root route

--- app.js
+++ app.js
@@ -201,3 +201,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
+app.get("/", (req, res) => {
+  res.send("API is running ✅");
+});
